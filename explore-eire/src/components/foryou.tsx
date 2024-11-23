6bc519cf41d82beb448bedeb10cd6f5de7c96c0d
Ignas Prak <ignasprak@gmail.com>
2024-11-23 19:08:18 +0000

    Add a clean popular section template with stock images and text and pseudo page selection in "for you" section

--- explore-eire/src/components/foryou.tsx
+++ explore-eire/src/components/foryou.tsx
@@ -8,7 +8,7 @@ import ForYouSection from "./foryou/foryousection";
 import HiddenGemsSection from "./foryou/hiddengemssection";
 
 export default function ForYou() {
-    const [selectedOption, setSelectedOption] = useState("For You");
+    const [selectedOption, setSelectedOption] = useState("Popular");
 
     const handleOptionClick = (option: string) => {
         setSelectedOption(option);
@@ -32,17 +32,33 @@ export default function ForYou() {
     };
 
     return (
-        <div className="bg-white p-6 w-11/12 h-auto rounded-lg mx-auto min-h-screen">
+        <div className="bg-white p-6 w-11/12 rounded-lg mx-auto h-auto inline-block mb-4">
+            {/* options to choose what section the user wants */}
             <div className="flex items-center justify-between mb-4 bg-gray-200 rounded-lg p-5">
-                <button className="text-gray-600 ml-5" onClick={() => handleOptionClick("Weather")}>Weather</button>
+                <button className="text-gray-600 ml-10" onClick={() => handleOptionClick("Weather")}>Weather</button>
                 <button className="text-gray-600 ml-5" onClick={() => handleOptionClick("Nearby")}>Nearby</button>
                 <button className="text-gray-600 mr-5" onClick={() => handleOptionClick("Popular")}>Popular</button>
                 <button className="text-gray-600 mr-5" onClick={() => handleOptionClick("For You")}>For You</button>
-                <button className="text-gray-600 mr-5" onClick={() => handleOptionClick("Hidden Gems")}>Hidden Gems</button>
+                <button className="text-gray-600 mr-10" onClick={() => handleOptionClick("Hidden Gems")}>Hidden Gems</button>
             </div>
-            <div className="flex flex-col border-blue-700 h-96">
+
+            {/* rendered content displaying results */}
+            <div className="flex flex-col border-blue-700 h-auto">
                 {renderContent()}
             </div>
+
+            {/* page selection */}
+            <div className="flex justify-center mt-6 rounded-lg">
+                {Array.from({ length: 9 }, (_, i) => (
+                    <button
+                        key={i + 1}
+                        className="w-16 h-16 bg-gray-300 text-gray-700 rounded-lg mx-1 flex items-center justify-center transform hover:scale-105 transition-transform duration-200"
+                    >
+                        {i + 1}
+                    </button>
+                ))}
+            </div>
+
         </div>
     );
 }
--- explore-eire/src/components/foryou/popularsection.tsx
+++ explore-eire/src/components/foryou/popularsection.tsx
@@ -1,50 +1,86 @@
 import Image from 'next/image';
-// import img1 from '../../../public/images/blarney.jpg';
-// import img2 from '../../../public/images/causeway.jpg';
+
 import img1 from '../../../public/images/cliffs_moher.jpg';
 import img2 from '../../../public/images/causeway.jpg';
+import img3 from '../../../public/images/blarney.jpg';
+
 const PopularSection = () => (
-    <>
-        <div className="grid grid-rows-3 grid-cols-2 gap-1 border-2 border-red-500">
+    <div className="h-[800px] overflow-y-auto">
+        <div className="grid grid-rows-4 grid-cols-2 gap-1">
             {/* the image represents half of the parent div on the left*/}
-            <div className="row-span-3 col-span-1 border-2 border-green-500 mt-2 mb-2">
+            <div className="row-span-4 col-span-1 mt-2 mb-2">
                 <Image src={img1} alt="Cliffs of Moher" layout="fixed" className='object-contain max-w-full rounded-lg' />
             </div>
 
             {/* the upper text box on the right of the div, represents half of the width of the parent div, and half the height of the parent div */}
-            <div className="row-span-1 col-span-1 border-2 border-green-500 rounded-lg w-full mt-2">
-                Cliffs of Moher
+            <div className="row-span-1 col-span-1 rounded-lg w-full mt-2 ml-2">
+                <h1>
+                    Cliffs of Moher
+                </h1>
             </div>
 
             {/* the lower text box on the right of the div, represents half of the width of the parent div, and half the height of the parent div */}
-            <div className="row-span-2 col-span-1 border-2 border-green-500 rounded-lg max-w-full mb-2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at diam in massa bibendum pretium a eget erat.
-                Vivamus nunc velit, ornare quis lorem sed, imperdiet venenatis elit. Nam ac consequat nibh, non mattis justo.
-                Nullam ornare convallis congue. Aliquam dapibus fermentum condimentum. Curabitur quis mollis tellus.
-                Aenean at nisi nec diam egestas posuere. Morbi mollis, enim in luctus iaculis, augue lectus mollis nisl,
-                egestas fermentum lorem massa a sapien. Ut accumsan enim metus, a ullamcorper nisl porttitor ut.
+            <div className="row-span-3 col-span-1 rounded-lg max-w-full mb-2 ml-2">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at diam in massa bibendum pretium a eget erat.
+                    Vivamus nunc velit, ornare quis lorem sed, imperdiet venenatis elit. Nam ac consequat nibh, non mattis justo.
+                    Nullam ornare convallis congue. Aliquam dapibus fermentum condimentum. Curabitur quis mollis tellus.
+                    Aenean at nisi nec diam egestas posuere. Morbi mollis, enim in luctus iaculis, augue lectus mollis nisl,
+                    egestas fermentum lorem massa a sapien. Ut accumsan enim metus, a ullamcorper nisl porttitor ut.
+                </p>
             </div>
         </div>
 
-        <div className="grid grid-rows-3 grid-flow-col gap-1 border-2 border-red-500">
-            {/* the image represents half of the parent div */}
-            <div className="row-span-3 border-2 border-green-500 w-1/2">
+        <div className="grid grid-rows-4 grid-cols-2 gap-1">
+            {/* the image represents half of the parent div on the left*/}
+            <div className="row-span-4 col-span-1 mt-2 mb-2">
                 <Image src={img2} alt="Giant's Causeway" layout="fixed" className='object-contain max-w-full rounded-lg' />
             </div>
-            {/* the upper text box represents half of the width of the parent div, and half the height of the parent div */}
-            <div className="row-span-1 col-span-1 border-2 border-green-500 rounded-lg w-full">Giant's Causeway</div>
-            {/* the lower text box represents half of the width of the parent div, and half the height of the parent div */}
-            <div className="row-span-2 col-span-1 border-2 border-green-500 rounded-lg max-w-full">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at diam in massa bibendum pretium a eget erat.
-                Vivamus nunc velit, ornare quis lorem sed, imperdiet venenatis elit. Nam ac consequat nibh, non mattis justo.
-                Nullam ornare convallis congue. Aliquam dapibus fermentum condimentum. Curabitur quis mollis tellus.
-                Aenean at nisi nec diam egestas posuere. Morbi mollis, enim in luctus iaculis, augue lectus mollis nisl,
-                egestas fermentum lorem massa a sapien. Ut accumsan enim metus, a ullamcorper nisl porttitor ut.
+
+            {/* the upper text box on the right of the div, represents half of the width of the parent div, and half the height of the parent div */}
+            <div className="row-span-1 col-span-1 rounded-lg w-full mt-2 ml-2">
+                <h1>
+                    Giant&apos;s Causeway
+                </h1>
+            </div>
+
+            {/* the lower text box on the right of the div, represents half of the width of the parent div, and half the height of the parent div */}
+            <div className="row-span-3 col-span-1 rounded-lg max-w-full mb-2 ml-2">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at diam in massa bibendum pretium a eget erat.
+                    Vivamus nunc velit, ornare quis lorem sed, imperdiet venenatis elit. Nam ac consequat nibh, non mattis justo.
+                    Nullam ornare convallis congue. Aliquam dapibus fermentum condimentum. Curabitur quis mollis tellus.
+                    Aenean at nisi nec diam egestas posuere. Morbi mollis, enim in luctus iaculis, augue lectus mollis nisl,
+                    egestas fermentum lorem massa a sapien. Ut accumsan enim metus, a ullamcorper nisl porttitor ut.
+                </p>
             </div>
         </div>
 
+        <div className="grid grid-rows-4 grid-cols-2 gap-1">
+            {/* the image represents half of the parent div on the left*/}
+            <div className="row-span-4 col-span-1 mt-2 mb-2">
+                <Image src={img3} alt="Blarney Castle" layout="fixed" className='object-contain max-w-full rounded-lg' />
+            </div>
 
-    </>
+            {/* the upper text box on the right of the div, represents half of the width of the parent div, and half the height of the parent div */}
+            <div className="row-span-1 col-span-1 rounded-lg w-full mt-2 ml-2">
+                <h1>
+                    Blarney Castle
+                </h1>
+            </div>
+
+            {/* the lower text box on the right of the div, represents half of the width of the parent div, and half the height of the parent div */}
+            <div className="row-span-3 col-span-1 rounded-lg max-w-full mb-2 ml-2">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at diam in massa bibendum pretium a eget erat.
+                    Vivamus nunc velit, ornare quis lorem sed, imperdiet venenatis elit. Nam ac consequat nibh, non mattis justo.
+                    Nullam ornare convallis congue. Aliquam dapibus fermentum condimentum. Curabitur quis mollis tellus.
+                    Aenean at nisi nec diam egestas posuere. Morbi mollis, enim in luctus iaculis, augue lectus mollis nisl,
+                    egestas fermentum lorem massa a sapien. Ut accumsan enim metus, a ullamcorper nisl porttitor ut.
+                </p>
+            </div>
+        </div>
+    </div>
 );
 
 export default PopularSection;
