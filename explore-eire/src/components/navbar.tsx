8fe53b027f7173d1d2f465b22128c659196ba6b0
Ignas Prak <ignasprak@gmail.com>
2024-11-23 20:17:59 +0000

    Update navbar with logo

--- explore-eire/src/components/navbar.tsx
+++ explore-eire/src/components/navbar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Image from 'next/image';
-import logo from 'https://github.com/ignasprak/explore-eire/blob/development/explore-eire/public/images/logo.png';
+import logo from '../../public/images/logo.png';
 
 function NavLink({ to, children }: { to: string, children: React.ReactNode }) {
     return <a href={to} className={`mx-4`}>
@@ -11,9 +11,10 @@ function NavLink({ to, children }: { to: string, children: React.ReactNode }) {
 
 export default function Navbar() {
     return (
-        <nav className="flex filter drop-shadow-md bg-white px-4 py-4 h-24 items-center">
+        <nav className="flex filter drop-shadow-md bg-white px-4 py-4 h-28 items-center">
             <div className="w-3/12 flex items-center">
-                <Image src={logo} alt="Logo" width={50} height={50} />
+                <Image src={logo} alt="The Explore Eire Logo" layout="fixed" className='m-0 max-w-32' />
+                {/* <Image src={img1} alt="Cliffs of Moher" layout="fixed" className='object-contain max-w-full rounded-lg' /> */}
             </div>
             <div className="w-6/12 flex justify-center items-center">
                 <p className="text-xl font-semibold mr-4 ">
